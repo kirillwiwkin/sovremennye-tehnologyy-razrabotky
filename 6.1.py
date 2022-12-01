63a3f48872814c3ecc52d8f37bd5c288f2506846
kirillwiwkin <114699953+kirillwiwkin@users.noreply.github.com>
2022-12-01 09:12:47 +0300

    Create 6.1.py

---
+++ 6.1.py
@@ -0,0 +1,12 @@
+# 4
+
+array = [5, 2, 5, 0, 2, 5, 8]
+
+max_val = max(array)
+max_val_idx = array.index(max_val)
+
+print(
+f"""Массив: {array}
+Максимальное значение: {max_val}
+Индекс максимального значения: {max_val_idx}"""
+)
