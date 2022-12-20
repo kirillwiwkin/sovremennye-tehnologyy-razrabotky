e7b575fd021ab610e806eac8cb633b762f7c03c4
kirillwiwkin <kirillshishkin7711@yandex.ru>
2022-12-20 11:42:54 +0300

    14

---
+++ 14.py
@@ -0,0 +1,17 @@
+# 4
+
+import csv
+
+with open("StudentsPerformance 14.csv") as f:
+    reader = csv.DictReader(f, skipinitialspace=True)
+    data = [r for r in reader]
+
+filteredData = list(
+    filter(
+        lambda x: x["race/ethnicity"] == "group C"
+        and x["test preparation course"] == "completed",
+        data,
+    )
+)
+
+print(filteredData)
